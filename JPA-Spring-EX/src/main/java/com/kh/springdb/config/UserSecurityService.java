06dd2fee5da626dbf463cb8600ff0e4d658e33a5
Yim-HaEun <49932613+Yim-HaEun@users.noreply.github.com>
2023-12-18 09:02:58 +0900

    ,

--- JPA-Spring-EX/src/main/java/com/kh/springdb/config/UserSecurityService.java
+++ JPA-Spring-EX/src/main/java/com/kh/springdb/config/UserSecurityService.java
@@ -41,7 +41,7 @@ public class UserSecurityService implements UserDetailsService{
 		SiteUser user = _siteUser.get();
 		List<GrantedAuthority> authorities = new ArrayList<>();
 		//만약에 admin user로 로그인된다면 로그인 분류를 role에 따라 추가로 작성
-		if("admin".equals(username)) {
+		if(UserRole.ADMIN.equals(user.getIsRole())) {
 			authorities.add(new SimpleGrantedAuthority(UserRole.ADMIN.getValue()));
 		}else {
 			authorities.add(new SimpleGrantedAuthority(UserRole.USER.getValue()));
